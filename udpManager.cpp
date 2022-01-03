903b18b30b039fb2478ea203a983018a6732de09
hanonymous89 <96468681+hanonymous89@users.noreply.github.com>
2022-01-03 20:36:12 +0900

    Add files via upload

--- udpManager.cpp
+++ udpManager.cpp
@@ -1,70 +1,106 @@
 #include <windows.h>
 #include <memory>
 #include <string>
+#include <unordered_map>
+#include <vector>
+#include <functional>
 #include <iostream>
-class WsaManager{
-    private:
-    WsaManager(){
-        WSAData wsaData;
-        WSAStartup(MAKEWORD(2,0),&wsaData);
-    };
-    virtual ~WsaManager(){
-        WSACleanup();
-    };
-    public:
-    WsaManager(const WsaManager&)=delete;
-    WsaManager& operator=(const WsaManager&)=delete;
-    WsaManager(WsaManager&&)=delete;
-    WsaManager& operator=(WsaManager&&)=delete;
-    static WsaManager& getInstance(){
-        static WsaManager wsaManager;
-        return wsaManager;
-    }
-};
-class UdpInterface{
-    protected:
-    static int count;
-    struct sockaddr_in addr;
-    int sock;
-    public:
-    UdpInterface(const decltype(ADDR_ANY) ip,const int port){
-        WsaManager::getInstance();
-        sock=socket(AF_INET,SOCK_DGRAM,0);
-        if(sock==INVALID_SOCKET){
-            return;
+#include <thread>
+namespace h{
+    class WsaManager{
+        private:
+        WsaManager(){
+            WSAData wsaData;
+            WSAStartup(MAKEWORD(2,0),&wsaData);
+        };
+        virtual ~WsaManager(){
+            WSACleanup();
+        };
+        public:
+        WsaManager(const WsaManager&)=delete;
+        WsaManager& operator=(const WsaManager&)=delete;
+        WsaManager(WsaManager&&)=delete;
+        WsaManager& operator=(WsaManager&&)=delete;
+        static WsaManager& getInstance(){
+            static WsaManager wsaManager;
+            return wsaManager;
         }
-        addr.sin_family=AF_INET;
-        addr.sin_port=htons(port);
-        addr.sin_addr.S_un.S_addr=ip;
-    }
-    virtual ~UdpInterface(){
-        if(sock==INVALID_SOCKET)return;
-        closesocket(sock);
-        sock=INVALID_SOCKET;
-    }
-};
-class UdpSend:private UdpInterface{
-    public:
-    UdpSend(const std::string ip,const int port):UdpInterface(inet_addr(ip.c_str()),port){
-    }
-    auto &send(const std::string data){
-        sendto(sock,data.c_str(),data.size()+1,0,(struct sockaddr*)&addr,sizeof(addr));
-        return *this;
-    }
-};
-class UdpServer:private UdpInterface{
-    public:
-    UdpServer(int port):UdpInterface(ADDR_ANY,port){
-        bind(sock,(struct sockaddr*)&addr,sizeof(addr));
-    }
-    std::string recv(int bufSize){
-        if(sock==INVALID_SOCKET)return "";
-        std::unique_ptr<char> buf(new char[bufSize]);
-        bufSize=::recv(sock,buf.get(),bufSize,0);
-        return std::string(buf.get(),bufSize);
-    }
+    };
+    class UdpInterface{
+        protected:
+        static int count;
+        struct sockaddr_in addr;
+        int sock;
+        public:
+        UdpInterface(const decltype(INADDR_ANY) ip,const int port){
+            WsaManager::getInstance();
+            sock=socket(AF_INET,SOCK_DGRAM,0);
+            if(sock==INVALID_SOCKET){
+                return;
+            }
+            addr.sin_family=AF_INET;
+            addr.sin_port=htons(port);
+            addr.sin_addr.S_un.S_addr=ip;
+        }
+        virtual ~UdpInterface(){
+            if(sock==INVALID_SOCKET)return;
+            closesocket(sock);
+            sock=INVALID_SOCKET;
+        }
+    };
+    class UdpSend:private UdpInterface{
+        public:
+        UdpSend(const std::string ip,const int port):UdpInterface(inet_addr(ip.c_str()),port){
+        }
+        auto &send(const std::string data){
+            if(sock==INVALID_SOCKET)return *this;
+            sendto(sock,data.c_str(),data.size()+1,0,(struct sockaddr*)&addr,sizeof(addr));
+            return *this;
+        }
+    };
+    class UdpServer:private UdpInterface{
+        public:
+        UdpServer(int port):UdpInterface(INADDR_ANY,port){
+            if(sock==INVALID_SOCKET)return;
+            bind(sock,(struct sockaddr*)&addr,sizeof(addr));
+        }
+        auto &blocking(bool is_Block){
+            u_long block=!is_Block;
+            ioctlsocket(sock,FIONBIO,&block);
+            return *this;
+        }
+        std::string recvfrom(const int bufSize){
+            if(sock==INVALID_SOCKET)return "";
+            std::unique_ptr<char> buf(new char[bufSize]);
+            struct sockaddr_in address;
+            int addlen=sizeof(address);
+            ::recvfrom(sock,buf.get(),bufSize,0,(struct sockaddr*)&address,&addlen);
+            return std::string(inet_ntoa(address.sin_addr))+"/"+std::to_string(ntohs(address.sin_port))+":"+std::string(buf.get());
+        }
+        std::string recv(const int bufSize){
+            if(sock==INVALID_SOCKET)return "";
+            std::unique_ptr<char> buf(new char[bufSize]);
+            if(::recv(sock,buf.get(),bufSize,0)==-1)return "";
+            return std::string(buf.get());
+        }
+    };
 };
 int main(){
-    std::cout<<UdpServer(46490).recv(100);
-    return system("pause");
+    h::UdpServer server(50000);
+    std::string input;
+    std::cout<<"ip(port:50000):";
+    std::cin>>input;
+    h::UdpSend client(input.c_str(),50000);
+    auto func=[&]{
+        while(input!="end"){
+            std::cout<<server.recvfrom(65535)<<std::endl;
+        }
+    };
+    std::thread thread(func);  
+    while(getline(std::cin,input)){
+        client.send(input);
+        if(input=="end")break;
+    }
+    thread.join();
+    return 0;
 }
